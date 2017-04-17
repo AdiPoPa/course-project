845c65de1704f3d39c2e529a2aa36c3876619b8b
Adrian Popa <adipopa9910@gmail.com>
2017-04-17 11:47:52 +0300

    Http requests added (/w firebase database)

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { RecipeService } from '../components/recipes/recipe.service';
+import { ShoppingListService } from '../components/shopping-list/shopping-list.service';
+import 'rxjs/Rx';
+import { Recipe } from '../components/recipes/recipe.model';
+import { AuthService } from '../components/auth/auth.service';
+import { promise } from 'selenium-webdriver';
+
+@Injectable()
+export class DataStorageService {
+
+  private recipesUrl = 'https://ng-course-project.firebaseio.com/recipes.json';
+  private shoppingListUrl = 'https://ng-course-project.firebaseio.com/shopping-list.json';
+
+  constructor(private http: Http, private recipeService: RecipeService, private shoppingListService: ShoppingListService,
+    private authService: AuthService) {}
+
+  storeRecipes() {
+    const token = this.authService.getToken();
+
+    return this.http.put(this.recipesUrl + '?auth=' + token, this.recipeService.getRecipes());
+  }
+
+  getRecipes() {
+    const token = this.authService.getToken();
+
+    this.http.get(this.recipesUrl + '?auth=' + token).map(
+      (response: Response) => {
+        const recipes: Recipe[] = response.json();
+        for (let recipe of recipes) {
+          if (!recipe['ingredients']) {
+            recipe['ingredients'] = [];
+          }
+        }
+        return recipes;
+      }
+    ).subscribe(
+      (recipes: Recipe[]) => {
+        this.recipeService.setRescipes(recipes);
+      }
+    );
+  }
+
+  storeShoppingList() {
+
+  }
+
+  getShoppingList() {
+
+  }
+
+}
